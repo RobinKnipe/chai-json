816a42f533d9ce955b9ba91a97b22473de950eda
Robin Knipe <robin.knipe@gmail.com>
2018-02-19 22:38:32 +0000

    Generify tests to support more node versions

--- test/chai-json.specs.js
+++ test/chai-json.specs.js
@@ -36,8 +36,8 @@ describe('chai-json', () => {
         expect('{a:1}').not.to.be.json;
         expect('[a:1]').not.to.be.json;
       });
-      it('should reject with the parse error', () => expect(() => expect('not json').to.be.json).to.throw(Error,
-        'expected \'not json\' to be a JSON formatted string, but got Unexpected token o in JSON at position 1'));
+      it('should reject with the parse error', () => expect(() => expect('not json').to.be.json)
+        .to.throw(Error, /^expected 'not json' to be a JSON formatted string, but got Unexpected token /));
     });
   });
 
@@ -83,7 +83,7 @@ describe('chai-json', () => {
       it('should reject with the parse error', () => expect(
         () => expect(badJSON).to.be.json.with.property('a', 'val'))
         .to.throw(Error,
-          `expected \'${badJSON}\' to be a JSON formatted string, but got Unexpected token p in JSON at position 27`));
+          new RegExp(`^expected \'${badJSON}\' to be a JSON formatted string, but got Unexpected token p`)));
     });
   });
 });
